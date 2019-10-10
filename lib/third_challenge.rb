0685c1ee52a4d9cf12ac03dca23b1826125bd4c5
Renata Freitas <r.miriuk@gmail.com>
2019-10-10 20:38:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,13 +19,13 @@ def third_challenge
      :matriarch => {name: "Lady Capulet", age: "51"},
      :heroine => {name: "Juliet", age: "15", status: "alive"},
      :heroine_friends => [{
-       :name => "Benvolio",
-       :age => "17",
-       :attitude => "worried"},
+       :name => "Steven",
+       :age => "30",
+       :attitude => "confused"},
        {
-         :name => "Mercutio",
-       :age => "18",
-       :attitude => "hot-headed"
+         :name => "Nurse",
+       :age => "44",
+       :attitude => "worried"
        }
        ]
    }
