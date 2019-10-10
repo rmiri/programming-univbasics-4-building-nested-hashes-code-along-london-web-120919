28640ead37992ffe769f81c691b2dc50fa7ca167
Renata Freitas <r.miriuk@gmail.com>
2019-10-10 20:28:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,9 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => {}
+     
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => {}
+
      }
    }
 end
